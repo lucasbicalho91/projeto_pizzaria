107d93eef694ecce310cdca977b9c6d9a20c0d03
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-01-25 19:50:42 -0300

    Criando a tela de login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,36 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/home.module.scss'
 
+import logoImg from '../../public/logo.svg'
+
+import { Input } from '../components/ui/input'
 
 export default function Home() {
   return (
-    <div>
-      <h1> Pizza </h1>
+    <>
+    <Head>
+      <title>Sujeito Pizza - Faça seu login</title>
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo sujeito Pizzaria" />
+
+      <div className={styles.login}>
+        <form>
+          <Input
+            placeholder="Digite seu email"
+            type="text"
+          />
+
+          <Input
+            placeholder="Sua senha"
+            type="password"
+          />
+
+        </form>
+      </div>
+
     </div>
+    </>
   )
 }
